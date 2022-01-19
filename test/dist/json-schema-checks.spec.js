6c51befc0801e7d79c187b0cca696f3f307f47bb
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2022-01-19 09:39:01 +0100

    test(dist): add tests for dist

---
+++ test/dist/json-schema-checks.spec.js
@@ -0,0 +1,26 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+'use strict'
+
+const { expect } = require('chai')
+
+const ccl = require('../../lib/ccl/ccl-main')
+
+describe('dist/json-schema-checks', () => {
+  context('check dist/ccl-configuration.json', () => {
+    const configuration = require('./../../dist/ccl-configuration.json')
+
+    it('is array', () => {
+      expect(configuration).to.be.an('array')
+    })
+
+    if (Array.isArray(configuration)) {
+      configuration.forEach((entry, idx) => {
+        it(`entry ${idx + 1} matches schema`, async () => {
+          const { errors } = await ccl.schema.validate(entry, 'https://ccl.coronawarn.app/ccl-configuration.json')
+          expect(errors).to.be.empty
+        })
+      })
+    }
+  })
+})
