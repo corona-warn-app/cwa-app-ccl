24509700cce473eda2fbe44e61f7acce8831a79e
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2022-02-10 11:26:17 +0100

    chore: fix jfn test case execution

--- test/unit/jfn/jfn-function-tests.spec.js
+++ test/unit/jfn/jfn-function-tests.spec.js
@@ -3,5 +3,22 @@
 import { executeFromFile } from './../../util/execute-jfn-test-case.js'
 
 describe('jfn/function-tests', () => {
-  executeFromFile('test/fixtures/jfn/jfn-function-tests/is-same-person.spec.json')
+  executeFromFile('test/fixtures/jfn/jfn-function-tests/is-same-person.spec.json', {
+    transform: data => data
+      .reduce((testCases, test) => {
+        return test.scenarios.reduce((testCases, scenario) => {
+          testCases.push({
+            title: `${test.title} - ${scenario.title}`,
+            functions: test.functions,
+            evaluateFunction: {
+              name: test.functionName,
+              parameters: scenario.parameters
+            },
+            exp: scenario.exp,
+            throws: scenario.throws
+          })
+          return testCases
+        }, testCases)
+      }, [])
+  })
 })
