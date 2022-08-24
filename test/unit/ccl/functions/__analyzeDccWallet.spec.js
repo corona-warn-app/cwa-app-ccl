c89dcad6c2cda71e32038e04cef72702987fefbc
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2022-08-24 09:37:54 +0200

    test(ccl): mask is always required in Hamburg

--- test/unit/ccl/functions/__analyzeDccWallet.spec.js
+++ test/unit/ccl/functions/__analyzeDccWallet.spec.js
@@ -35,7 +35,7 @@ describe('ccl/functions/__analyzeDccWallet', async () => {
 
       seriesDescriptor.testCases.forEach((testCase, idx) => {
         const _context = testCase.only === true ? context.only : context
-        const testCaseDescription = `test case #${idx + 1} at ${testCase.time} - ${testCase.description || ''}`
+        const testCaseDescription = `test case #${idx + 1} at ${testCase.time} - ${testCase.description || ''} - scenario '${testCase.scenarioIdentifier || ''}'`
         _context(testCaseDescription, () => {
           let timeUnderTest, seriesUnderTest
           let input, output
@@ -58,6 +58,7 @@ describe('ccl/functions/__analyzeDccWallet', async () => {
               boosterNotificationRules: [],
               invalidationRules: seriesDescriptor.invalidationRules || allIRs
             }
+            if (typeof testCase.scenarioIdentifier === 'string') input.scenarioIdentifier = testCase.scenarioIdentifier
 
             output = ccl.evaluateFunction('__analyzeDccWallet', input)
           })
