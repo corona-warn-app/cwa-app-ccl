bdfc26ff9248d5ac671306a200b587d8e1aaedd6
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2022-09-14 14:45:00 +0200

    Revert "chore(build): apply post-processing to chunks"
    This reverts commit e5273b850d2eb5c5546afbcc129b84e1fb9713f5.

--- scripts/post-process.js
+++ scripts/post-process.js
@@ -5,14 +5,6 @@ import fs from 'fs-extra'
 // processing.
 // For that purpose, we replace `.000Z` by `Z` to remove
 // milliseconds from ISO 8601 date-time representations.
-const filenames = [
-  'android-ccl-test-cases.gen.json',
-  'android-ccl-test-cases.gen.json.chunk0',
-  'android-ccl-test-cases.gen.json.chunk1',
-  'android-ccl-test-cases.gen.json.chunk2'
-]
-filenames.forEach(filename => {
-  const contents = fs.readFileSync(`./dist/${filename}`, 'utf-8')
-  const newContents = contents.replace(/\.000Z/g, 'Z')
-  fs.writeFileSync(`./dist/${filename}`, newContents)
-})
+const contents = fs.readFileSync('./dist/android-ccl-test-cases.gen.json', 'utf-8')
+const newContents = contents.replace(/\.000Z/g, 'Z')
+fs.writeFileSync('./dist/android-ccl-test-cases.gen.json', newContents)
